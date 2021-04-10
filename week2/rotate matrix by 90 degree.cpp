c11f1f0cf02ff508a2bfbddba0f2c19211914902
bhargavi2901 <81483979+bhargavi2901@users.noreply.github.com>
2021-04-10 18:35:14 +0530

    Create rotate matrix by 90 degree.cpp

---
+++ week2/rotate matrix by 90 degree.cpp
@@ -0,0 +1,20 @@
+void Solution::rotate(vector<vector<int> > &A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    for(int i=0;i<A.size();i++)
+    {
+        for(int j=i+1;j<A[0].size();j++)
+        {
+            int temp=A[i][j];
+            A[i][j]=A[j][i];
+            A[j][i]=temp;
+        }
+    }
+     for(int i=0;i<A.size();i++)
+     {
+         reverse(A[i].begin(),A[i].end());
+     }
+    
+}
