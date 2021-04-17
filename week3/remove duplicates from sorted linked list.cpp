54b26f6bce425fdeba595842aa4049206b0513ba
bhargavi2901 <81483979+bhargavi2901@users.noreply.github.com>
2021-04-17 18:46:25 +0530

    Create remove duplicates from sorted linked list.cpp

---
+++ week3/remove duplicates from sorted linked list.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode *temp=head;
+        while(head&&head->next!=NULL)
+        {  
+            if(head->val==head->next->val)
+            {
+                head->next=head->next->next;
+            }
+         else
+             head=head->next;
+        }
+        return temp;
+    }
+};
