9c7052a397e24c420773cb6feb7c43d85e6c7e95
bhargavi2901 <81483979+bhargavi2901@users.noreply.github.com>
2021-06-17 12:10:37 +0530

    Create floyd warshall.cpp

---
+++ week7/floyd warshall.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+	void shortest_distance(vector<vector<int>>&matrix){
+	    // Code here
+	    for(int k=0;k<matrix.size();k++)
+	    {
+	    for(int i=0;i<matrix.size();i++)
+	    {
+	        if(i!=k)
+	        {
+	        for(int j=0;j<matrix.size();j++)
+	        {
+	            if(j!=k && matrix[i][k]!=-1 &&matrix[k][j]!=-1)
+	            {
+	                if(matrix[i][j]==-1 || matrix[i][j]>matrix[i][k]+matrix[k][j])
+	                   matrix[i][j]=matrix[i][k]+matrix[k][j];
+	            }
+	        }
+	    }}}
+	  
+	}
+};
